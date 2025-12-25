2abf1a2123341781cb12a1457c900f8e1b533406
Destu Cikal <destucr@gmail.com>
2025-12-25 20:41:41 +0700

    fix: revert react snap installation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
-const container = document.getElementById('root');
-
-const app = (
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
@@ -16,10 +15,3 @@ const app = (
     </HelmetProvider>
   </React.StrictMode>
 );
-
-if (container.hasChildNodes()) {
-  hydrateRoot(container, app);
-} else {
-  const root = createRoot(container);
-  root.render(app);
-}
